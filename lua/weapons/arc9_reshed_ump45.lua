5056bda24db77c9a1b99483057756a31ce0d20b7
NotTrashraD <reski16@outlook.fr>
2023-12-16 04:36:48 +0100

    first att + sounds + model pdates

---
+++ lua/weapons/arc9_reshed_ump45.lua
@@ -0,0 +1,380 @@
+
+
+
+AddCSLuaFile()
+
+SWEP.Base = "arc9_base"
+
+SWEP.Spawnable = true
+SWEP.Category = "ARC9 - Bôlemos"
+SWEP.SubCategory = "SMGS"
+SWEP.AdminOnly = false
+
+
+SWEP.PrintName = "H&k UMP45"
+
+
+SWEP.Class = "Sub Machine Gun"
+
+SWEP.Trivia = {} 
+
+SWEP.Credits = {}
+
+SWEP.Description = [[Description Unavailable.]]
+
+SWEP.UseHands = true -- Same as weapon_base
+
+SWEP.ViewModel = "models/reshed_arc9_ump45.mdl"
+
+SWEP.WorldModel = "models/weapons/w_smg_ump45.mdl"
+
+SWEP.MirrorVMWM = true
+
+SWEP.WorldModelOffset = {
+    Pos = Vector(0, 0, 0), -- non tpik (while on ground, on npc etc)
+     Ang = Angle(0, 0, 0),
+     TPIKPos = Vector(-5, 5, -7), -- arc9_tpik 1, you can make cool poses with it
+    TPIKAng = Angle(0, 0, 180),
+     Scale = 0.9
+}
+
+
+SWEP.Crosshair = true
+
+
+
+SWEP.DamageMax = 24 -- Damage done at point blank range
+SWEP.DamageMin = 17 -- Damage done at maximum range
+
+
+SWEP.ImpactForce = 0 -- Force that bullets apply on hit
+
+SWEP.DamageRand = 0 -- Damage varies randomly per shot by this fraction. 0.1 = +- 10% damage per shot.
+
+
+SWEP.RangeMin = 0 -- How far bullets retain their maximum damage for.
+SWEP.RangeMax = 5000 -- In Hammer units, how far bullets can travel before dealing DamageMin.
+SWEP.Distance = 33000 -- In Hammer units, how far bullets can travel, period.
+
+
+SWEP.Num = 1 -- Number of bullets to shoot
+
+SWEP.Penetration = 4 -- Units of wood that can be penetrated by this gun.
+
+
+SWEP.HeadshotDamage = 1
+SWEP.ChestDamage = 1
+SWEP.StomachDamage = 1
+SWEP.ArmDamage = 1
+SWEP.LegDamage = 1
+
+
+SWEP.NeverPhysBullet = true
+
+
+SWEP.Ammo = "pistol" -- What ammo type this gun uses.
+
+SWEP.ChamberSize = 1 -- The amount of rounds this gun can chamber.
+SWEP.ClipSize = 30 -- Self-explanatory.
+
+
+SWEP.RPM = 700
+
+SWEP.Firemodes = {
+    {
+        Mode = -1,
+        -- add other attachment modifiers
+    },
+    {
+        Mode = 1,
+        -- add other attachment modifiers
+    }
+}
+
+
+SWEP.Recoil = 3
+
+
+SWEP.RecoilUp = 1 -- Multiplier for vertical recoil
+SWEP.RecoilSide = 0.5 -- Multiplier for vertical recoil
+
+
+SWEP.RecoilRandomUp = 0
+SWEP.RecoilRandomSide = 0
+
+
+
+
+SWEP.Spread = 0.003
+
+
+SWEP.SpreadAddMove = 0.01 -- Applied when speed is equal to walking speed.
+SWEP.SpreadAddMidAir = 0.5 -- Applied when not touching the ground.
+SWEP.SpreadAddHipFire = 0.010 -- Applied when not sighted.
+SWEP.SpreadAddSighted = -0.003 -- Applied when sighted. Can be negative.
+SWEP.SpreadAddBlindFire = nil -- Applied when blind firing.
+SWEP.SpreadAddCrouch = 0 -- Applied when crouching.
+
+
+SWEP.FreeAimRadius = 0 -- In degrees, how much this gun can free aim in hip fire.
+SWEP.Sway = 0.5 -- How much the gun sways.
+
+SWEP.HoldBreathTime = 4 -- time that you can hold breath for, set to 0 to disable holding breath
+SWEP.RestoreBreathTime = 8
+
+SWEP.FreeAimRadiusMultSights = 0
+
+SWEP.SwayMultSights = 0
+
+SWEP.AimDownSightsTime = 0.15 -- How long it takes to go from hip fire to aiming down sights.
+SWEP.SprintToFireTime = 0.15 -- How long it takes to go from sprinting to being able to fire.
+
+
+SWEP.CanLean = false
+
+
+-------------------------- SOUNDS
+
+SWEP.ShootVolume = 125
+SWEP.ShootVolumeActual = 1
+SWEP.ShootPitch = 100
+SWEP.ShootPitchVariation = 5 -- Not multiplied, but actually just added/subtracted.
+
+
+SWEP.ShootSound = "weapons/ump45/fire.wav"                            -- Fire
+
+SWEP.ShootSoundSilenced = "weapons/ump45/fire_sil.wav"                    -- Fire silenced
+
+
+
+
+
+
+
+
+
+
+
+SWEP.FiremodeSound = "arc9/firemode.wav"
+SWEP.ToggleAttSound = {
+    "arc9/toggles/flashlight_laser_toggle_on_01.ogg",
+    "arc9/toggles/flashlight_laser_toggle_on_02.ogg",
+    "arc9/toggles/flashlight_laser_toggle_on_03.ogg",
+}
+
+
+
+SWEP.BreathInSound = "arc9/breath_inhale.wav"
+SWEP.BreathOutSound = "arc9/breath_exhale.wav"
+SWEP.BreathRunOutSound = "arc9/breath_runout.wav"
+
+SWEP.TriggerDownSound = ""
+SWEP.TriggerUpSound = ""
+
+SWEP.MuzzleParticle = "MuzzleFlash_6"
+SWEP.AfterShotParticle = "EjectBrass_556"
+
+SWEP.AfterShotEffect = "EjectBrass_556"
+
+
+SWEP.ShellModel = "models/shells/shell_9mm.mdl"
+
+SWEP.ShellSmoke = true
+
+
+SWEP.ShellPitch = 100 -- for shell sounds
+SWEP.ShellSounds = ARC9.ShellSoundsTable
+
+SWEP.ShellCorrectPos = Vector(0, 0, 0)
+SWEP.ShellCorrectAng = Angle(0, 0, 0)
+SWEP.ShellVelocity = nil -- nothing for random, otherwise keep this 0 - 2
+SWEP.ShellTime = 0.5 -- Extra time these shells stay on the ground for.
+
+SWEP.ShellScale = 2
+
+
+
+SWEP.MuzzleEffectQCA = 1 -- QC Attachment that controls muzzle effect.
+SWEP.AfterShotQCA = 1 -- QC Attachment that controls after shot particle.
+SWEP.CaseEffectQCA = 2 -- QC Attachment for shell ejection.
+SWEP.CamQCA = 1 -- QC Attachment for camera movement.
+SWEP.CamCoolView = true -- Enable to use procedural camera movement. Set CamQCA to muzzle QCA or something.
+
+SWEP.CamQCA_Mult = 0.5
+
+
+SWEP.IronSights = {
+    Pos = Vector(-4.42, -6, 1.2),
+    Ang = Angle(0, 0.5, 0.2),
+    Magnification = 1,
+    AssociatedSlot = 0, -- Attachment slot to associate the sights with. Causes RT scopes to render.
+    CrosshairInSights = false,
+    Blur = true, -- If arc9_fx_adsblur 1 then blur gun in that ironsights. Disable if your "ironsights" are not real ironsights
+    ---- FLAT SCOPES
+    -- These don't look very good; please use actual RT scopes if possible.
+    FlatScope = false,
+    FlatScopeOverlay = nil, -- Material()
+    FlatScopeKeepVM = false,
+    FlatScopeBlackBox = true,
+    FlatScopeCC = nil -- Color correction table, see default.lua
+}
+
+SWEP.SightMidPoint = { -- Where the gun should be at the middle of it's irons
+    Pos = Vector(0, 0, 0),
+    Ang = Angle(0, 0, 0),
+}
+
+SWEP.ViewModelFOVBase = 80 -- Set to override viewmodel FOV
+-- Alternative "resting" position
+SWEP.ActivePos = Vector(-1, -5, 0)
+SWEP.ActiveAng = Angle(0, 0, -5)
+
+-- Position while walking/running (no sprint)
+SWEP.MovingPos = nil -- Vector(0, 0, 0)
+SWEP.MovingAng = nil -- Angle(0, 0, 0)
+
+-- Position when crouching
+SWEP.CrouchPos = Vector(-4, 1, -4)
+SWEP.CrouchAng = Angle(0, 0, -30)
+
+-- Position when sprinting or safe
+SWEP.RestPos = Vector(0, -5, 0)
+SWEP.RestAng = Angle(23, -10, -30)
+
+SWEP.CustomizeAng = Angle(90, 0, 0)
+SWEP.CustomizePos = Vector(20, 32, 4)
+SWEP.CustomizeRotateAnchor = Vector(21.5, -4.27, -5.23)
+SWEP.CustomizeSnapshotFOV = 90
+SWEP.CustomizeSnapshotPos = Vector(0, 0, 0)
+SWEP.CustomizeSnapshotAng = Angle(0, 0, 0)
+SWEP.CustomizeNoRotate = false
+
+
+SWEP.PeekPos = Vector(-1, 0, -4.5)
+SWEP.PeekAng = Angle(0, 0.4, -35)
+
+SWEP.HeightOverBore = 1
+
+
+-------------------------- HoldTypes
+
+SWEP.HoldType = "ar2"
+SWEP.HoldTypeSprint = "ar2"
+SWEP.HoldTypeHolstered = nil
+SWEP.HoldTypeSights = "rpg"
+SWEP.HoldTypeCustomize = "slam"
+
+SWEP.AnimShoot = ACT_HL2MP_GESTURE_RANGE_ATTACK_AR2
+SWEP.AnimReload = ACT_HL2MP_GESTURE_RELOAD_MAGIC -- While in TPIK only -- Tip: if you dont want any additional anim put ACT_HL2MP_GESTURE_RELOAD_MAGIC here instead!
+SWEP.NonTPIKAnimReload = ACT_HL2MP_GESTURE_RELOAD_AR2 -- Non TPIK
+
+
+SWEP.Attachments = {
+    {
+        PrintName = "Top Rail",
+        DefaultAttName = "No Device",
+        Category = "ins2_opticrail",
+        Bone = "Weapon",
+        Pos = Vector(0, -0.2, 0),
+        Ang = Angle(90, -90, 0),
+    },
+   {
+        PrintName = "Bottom Rail",
+        DefaultAttName = "No Device",
+        Category = "ins2_rail",
+        Bone = "Weapon",
+        Pos = Vector(0, 5, 10),
+        Ang = Angle(90, -90, 0),
+    },
+}
+
+SWEP.Animations = {
+    ["fire"] = {
+        Source = "fire",
+		Time = 0.45,
+    },
+	
+    ["inspect"] = {
+        Source = "inspect",
+			EventTable = {
+		    {s = "weapons/shared/deploy.wav", t = 0},
+			{s = "weapons/ump45/magrelease.wav", t = 1.9},
+			{s = "weapons/ump45/magout.wav", t = 2},
+			{s = "weapons/shared/reloadend.wav", t = 3.1},
+			{s = "weapons/shared/reloadstart.wav", t = 4.2},
+			{s = "weapons/ump45/maghit.wav", t = 4.7},
+			{s = "weapons/ump45/magin.wav", t = 4.8},
+			{s = "weapons/shared/reloadstart.wav", t = 5.2},
+			{s = "weapons/shared/deploy.wav", t = 6.5},
+			},
+        },
+    ["reload"] = {
+        Source = "reload",
+        IKTimeLine = {
+            {
+                t = 0,
+                lhik = 1,
+                rhik = 0
+            },
+            {
+                t = 0.1,
+                lhik = 0,
+                rhik = 0
+            },
+            {
+                t = 0.75,
+                lhik = 0,
+                rhik = 0
+            },
+            {
+                t = 1,
+                lhik = 1,
+                rhik = 1
+            },
+        },
+        EventTable = {
+		    {s = "weapons/shared/reloadstart.wav", t = 0},
+		    {s = "weapons/ump45/magrelease.wav", t = 0.17},
+			{s = "weapons/ump45/magout.wav", t = 0.25},
+			{s = "weapons/shared/deploy.wav", t = 1.1},
+			{s = "weapons/ump45/maghit.wav", t = 1.55},
+			{s = "weapons/ump45/magin.wav", t = 1.65},
+			{s = "weapons/shared/reloadend.wav", t = 1.99}
+        },
+    },
+    ["reload_empty"] = {
+        Source = "reload_empty",
+               IKTimeLine = {
+            {
+                t = 0,
+                lhik = 1,
+                rhik = 0
+            },
+            {
+                t = 0.1,
+                lhik = 0,
+                rhik = 0
+            },
+            {
+                t = 0.85,
+                lhik = 0,
+                rhik = 0
+            },
+            {
+                t = 1,
+                lhik = 1,
+                rhik = 1
+            },
+        },
+        EventTable = {
+		    {s = "weapons/shared/reloadstart.wav", t = 0},
+		    {s = "weapons/ump45/magrelease.wav", t = 0.17},
+			{s = "weapons/ump45/magout_empty.wav", t = 0.25},
+			{s = "weapons/shared/deploy.wav", t = 1.1},
+			{s = "weapons/ump45/maghit.wav", t = 1.55},
+			{s = "weapons/ump45/magin.wav", t = 1.65},
+			{s = "weapons/ump45/boltcatch.wav", t = 2.1},
+			{s = "weapons/shared/reloadend.wav", t = 2.5}
+        },
+    }
+}
